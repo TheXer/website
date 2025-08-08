966d1a034ea7f4a9f45f191e5871161d4f0d866f
z004rmyh <robert.sokola@siemens.com>
2025-08-08 13:00:10 +0200

    add more content

--- src/consts.ts
+++ src/consts.ts
@@ -5,7 +5,7 @@ export const SITE: Site = {
   EMAIL: "robertsokola2@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 1,
+  NUM_PROJECTS_ON_HOMEPAGE: 2,
   YEAR: new Date().getFullYear(), // Automatically set the current year
 };
 
