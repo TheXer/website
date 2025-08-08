95d5ae45086ba368dc4e7edbe7239c21da4e6c0c
z004rmyh <robert.sokola@siemens.com>
2025-08-08 09:18:51 +0200

    analytics fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  adapter: vercel({
+      webAnalytics: {
+        enabled: true,
+      },
+    }),
 });
